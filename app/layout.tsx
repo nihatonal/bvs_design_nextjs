6ff2956be2263b8f8d6740a70d78a0b0fccc434c
nihatonal <onalnihat1986@gmail.com>
2025-07-14 19:47:13 +0300

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// const geistSans = localFont({
-//   variable: "--font-geist-sans",
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = localFont({
-//   variable: "--font-geist-mono",
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={` antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-  // diğer ayarlar
-};
-
-module.exports = config;
